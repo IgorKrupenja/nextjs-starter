abba9630d5bede329793e968a24799dbdda0b4fc
Igor Krupenja <igor.krupenja@gmail.com>
2024-02-17 01:09:12 +0200

    Add coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,21 @@
 import path from 'path';
 
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      exclude: [
+        ...(configDefaults.coverage.exclude ?? []),
+        'next.config.js',
+        'postcss.config.cjs',
+        'tailwind.config.ts',
+      ],
+    },
   },
   resolve: {
     alias: {
