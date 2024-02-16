7ea46217e9d52449f8d1c8d4bb223f0bc0bdcf16
Igor Krupenja <igor.krupenja@gmail.com>
2024-02-16 22:39:51 +0200

    Use edge runtime on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import { ReactElement } from 'react';
 
+export const runtime = 'edge';
+
 import { ChatForm } from '@/components/forms/chat-form';
 import {
   Accordion,
