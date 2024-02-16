e666dd0a52d84fcf9d8d5fd4fb171c8722c7c7e2
Igor Krupenja <igor.krupenja@gmail.com>
2024-02-17 01:49:47 +0200

    Make icon smaller

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { ChatForm } from '@/components/forms/chat-form';
 export default function Home(): ReactElement {
   return (
     <main className="flex w-full flex-col items-center justify-between">
-      <Rocket size={100} name="Rocket" />
+      <Rocket size={100} />
       <ChatForm className="m-4" />
     </main>
   );
