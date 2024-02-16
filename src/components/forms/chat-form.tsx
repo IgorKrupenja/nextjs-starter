e52edf1d7667f7cabf227ccc89d9d09144b614c9
Igor Krupenja <igor.krupenja@gmail.com>
2024-02-16 23:58:47 +0200

    Add test button

--- src/components/forms/chat-form.tsx
+++ src/components/forms/chat-form.tsx
@@ -3,6 +3,8 @@
 import { useChat } from 'ai/react';
 import { ReactElement } from 'react';
 
+import { Button } from '@/components/ui/button';
+
 export const ChatForm = (): ReactElement => {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
@@ -17,6 +19,19 @@ export const ChatForm = (): ReactElement => {
       <form onSubmit={handleSubmit}>
         <input value={input} placeholder="Say something..." onChange={handleInputChange} />
       </form>
+      <Button
+        onClick={() =>
+          void fetch('https://dummy.restapiexample.com/api/v1/create', {
+            method: 'POST',
+            body: JSON.stringify({ name: 'test', salary: '123', age: '23' }),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          })
+        }
+      >
+        test post
+      </Button>
     </div>
   );
 };
