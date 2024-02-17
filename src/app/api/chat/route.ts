b2d261696f04eda316df491c37b5d4ca4dff3366
Igor Krupenja <igor.krupenja@gmail.com>
2024-02-17 18:10:04 +0200

    Change GPT model

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -11,7 +11,7 @@ export async function POST(request: Request): Promise<Response> {
   };
 
   const response = await openai.chat.completions.create({
-    model: 'gpt-4',
+    model: 'gpt-3.5-turbo',
     stream: true,
     messages,
   });
