dae706971a43897ae5cf6bf3f9d894be4553e589
Igor Krupenja <igor.krupenja@gmail.com>
2024-02-17 17:53:15 +0200

    Better validation

--- src/app/api/feedback/route.ts
+++ src/app/api/feedback/route.ts
@@ -1,9 +1,16 @@
+import { getErrorsFromParseResult } from '@/lib/utils';
 import { FeedbackFormValues, feedbackFormSchema } from '@/schemas/feedback';
 
 export async function POST(request: Request): Promise<Response> {
   const { name, eMail, message } = (await request.json()) as FeedbackFormValues;
 
-  feedbackFormSchema.safeParse({ name, eMail, message });
+  const parseResult = feedbackFormSchema.safeParse({ name, eMail, message });
+
+  const errors = getErrorsFromParseResult(parseResult);
+
+  if (Object.keys(errors).length > 0) {
+    return Response.json({ errors }, { status: 400 });
+  }
 
   console.log({ name, eMail, message });
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { SafeParseReturnType } from 'zod';
 
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
+}
+
+export function getErrorsFromParseResult<T>(
+  parseResult: SafeParseReturnType<T, T>,
+): Record<string, string> {
+  if (parseResult.success) {
+    return {};
+  }
+
+  return parseResult.error.issues.reduce<Record<string, string>>((acc, issue) => {
+    return {
+      ...acc,
+      [issue.path.join('.')]: issue.message,
+    };
+  }, {});
 }
