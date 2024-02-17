9ae3ff5774ee25bc9c80318b1f2c69380e430786
Igor Krupenja <igor.krupenja@gmail.com>
2024-02-17 15:54:20 +0200

    Add heading

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -5,23 +5,8 @@ import { FeedbackForm } from '@/components/feedback/feedback-form';
 export default function FeedbackPage(): ReactElement {
   return (
     <>
+      <h1 className="mb-10 ml-auto mr-0 text-4xl font-extrabold">Let&apos;s get in touch!</h1>
       <FeedbackForm />
-      {/* TODO: Replace with a better example */}
-      {/* TODO: Also use form, like for feedback? */}
-      {/* <Button
-        className="mt-4"
-        onClick={() =>
-          void fetch('/api/chat', {
-            method: 'POST',
-            body: JSON.stringify({ name: 'test', salary: '123', age: '23' }),
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          })
-        }
-      >
-        Test POST, bad request
-      </Button> */}
     </>
   );
 }
