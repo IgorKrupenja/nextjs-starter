5df381f53d0eff9fb685724a79b09e81deec52e2
Igor Krupenja <igor.krupenja@gmail.com>
2024-02-17 13:51:15 +0200

    Clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ import { Chat } from '@/components/chat/chat';
 export default function Home(): ReactElement {
   return (
     <>
-      <Rocket size={100} role="img" aria-label="Logo of a rocket" className="text-primary" />
+      <Rocket size={100} role="img" aria-label="Logo of a rocket" className="text-foreground" />
 
       <Chat className="m-4 w-full" />
 
       <span className="text-sm">
         Have feedback?{' '}
-        <Link className="underline hover:text-primary" href="/feedback">
+        <Link className="underline hover:text-foreground" href="/feedback">
           Get in touch!
         </Link>
       </span>
