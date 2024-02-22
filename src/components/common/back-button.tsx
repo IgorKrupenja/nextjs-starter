63b8d8b642e0bf06b82e16612e8a1e23f6c6bb9d
Igor Krupenja <igor.krupenja@gmail.com>
2024-02-23 00:54:41 +0200

    Improve a11y

--- src/components/common/back-button.tsx
+++ src/components/common/back-button.tsx
@@ -4,11 +4,10 @@ import { ReactElement } from 'react';
 
 import { Button } from '@/components/ui/button';
 import { PropsWithClassName } from '@/interfaces/props-with-class-name';
-import { cn } from '@/lib/utils';
 
 export default function BackButton({ className }: PropsWithClassName): ReactElement {
   return (
-    <Button asChild variant="outline" className={cn('text-primary hover:text-primary', className)}>
+    <Button asChild variant="outline" className={className}>
       <Link href="/">
         <ArrowLeft className="mr-2 h-5 w-5" />
         Back
